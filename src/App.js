2a6386e9aeee01c25a8adaca8878e7030cee2a74
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-27 18:34:42 +0100

    create the profile section and create Data.jsx componenent for storing links data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 
 function App() {
   return (
-    
-    <>
-    <p className="underline">set up the react page</p>
-    </>
+    <main>
+      <section className="flex flex-col justify-center items-center">
+        <img src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4" alt="profile" id="profile-id" className="w-[40px] h-[40px] rounded-full"/>
+        
+        <p className="text-sm font-bold block mt-[10px]">Eze Williams</p>
+        
+      </section>
+    </main>
   );
 }
 
---
+++ src/Data.jsx
@@ -0,0 +1,31 @@
+const data = [
+{
+link:"https://twitter.com/mysticwillz",
+linkName:"Twitter Link"
+},
+{
+link:"https://training.zuri.team/",
+linkName:"Zuri Team",
+id:"btn__zuri"
+},
+{
+link:"http://books.zuri.team",
+linkName:"Zuri Books",
+id:"books"
+},
+{
+link:"https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
+linkName:"python books"
+},
+{
+link:"https://background.zuri.team",
+linkName:"background check for coders",
+id:"pitch"
+},
+{
+link:"https://books.zuri.team/design-rules",  
+linkName:"design books",
+id:"book__design"
+}
+
+]
