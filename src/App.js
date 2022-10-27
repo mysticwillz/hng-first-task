b3797f6e98212224ac7c1e8b8fb30f48ee31edf2
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-27 19:13:39 +0100

    style the button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@
 function App() {
   return (
     <main>
-      <section className="flex flex-col justify-center items-center">
+      <section className="flex flex-col justify-center items-center mt-[25px]">
         <img src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4" alt="profile" id="profile-id" className="w-[40px] h-[40px] rounded-full"/>
         
         <p className="text-sm font-bold block mt-[10px]">Eze Williams</p>
+        
+      </section>
+      <section className="max-w-5xl flex flex-col justify-center mx-auto p-4 items-center">
+      
+        <button className=" w-full p-4 bg-gray-300 my-[4px] h-[30px] flex items-center justify-center capitalize rounded text-sm "> twitter page </button>
         
       </section>
     </main>
