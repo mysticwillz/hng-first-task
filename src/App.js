5d544eb7cf38d90126573626507b785eed4b55a4
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-29 00:28:53 +0100

    create pitch SUBtext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-
+import data from "./Data";
+import { Link, redirect } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [showText, setShowText] = useState(false);
+  const [showRedirect, setShowRedirect] = useState(false);
   return (
     <main>
       <section className="flex flex-col justify-center items-center mt-[25px]">
-        <img src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4" alt="profile" id="profile-id" className="w-[40px] h-[40px] rounded-full"/>
-        
+        <img
+          src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4"
+          alt="profile"
+          id="profile-id"
+          className="w-[40px] h-[40px] rounded-full"
+        />
+
         <p className="text-sm font-bold block mt-[10px]">Eze Williams</p>
-        
       </section>
-      <section className="max-w-5xl flex flex-col justify-center mx-auto p-4 items-center">
-      
-        <button className=" w-full p-4 bg-gray-300 my-[4px] h-[30px] flex items-center justify-center capitalize rounded text-sm "> twitter page </button>
-        
+      <section className="max-w-4xl flex flex-col justify-center mx-auto p-4 items-center">
+        {data.map((x) => {
+          const { link, linkName, id, pitchText, redirect } = x;
+
+          const handleClicked = (clickedId) => {
+            if (clickedId === "pitch") {
+              setShowText(!showText);
+              setShowRedirect(!showRedirect);
+            } else {
+              <Link to={link} />;
+            }
+          };
+
+          return (
+            <>
+              <button
+                key={id}
+                className=" w-full p-4 bg-gray-300 my-[4px] h-[30px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]"
+                onClick={() => {
+                  handleClicked(id);
+                }}
+              >
+                {linkName}
+              </button>
+              {showText && (
+                <p>
+                  {pitchText}
+                  {redirect && (
+                    <span className="w-[5.3rem] h-[2.2rem] whitespace-nowrap cursor-pointer rounded-sm flex items-center justify-center bg-red-500 hover:bg-red-700 text-white capitalize transition duration-250 all ease-in-out my-[1.5px]">
+                      {redirect}
+                    </span>
+                  )}
+                </p>
+              )}
+            </>
+          );
+        })}
       </section>
     </main>
   );
--- src/Data.jsx
+++ src/Data.jsx
@@ -1,31 +1,45 @@
 const data = [
-{
-link:"https://twitter.com/mysticwillz",
-linkName:"Twitter Link"
-},
-{
-link:"https://training.zuri.team/",
-linkName:"Zuri Team",
-id:"btn__zuri"
-},
-{
-link:"http://books.zuri.team",
-linkName:"Zuri Books",
-id:"books"
-},
-{
-link:"https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
-linkName:"python books"
-},
-{
-link:"https://background.zuri.team",
-linkName:"background check for coders",
-id:"pitch"
-},
-{
-link:"https://books.zuri.team/design-rules",  
-linkName:"design books",
-id:"book__design"
-}
+  {
+    link: "https://twitter.com/mysticwillz",
+    linkName: "Twitter Link",
+    id: "twitter_btn",
+  },
+  {
+    link: "https://training.zuri.team/",
+    linkName: "Zuri Team",
+    id: "btn__zuri",
+  },
+  {
+    link: "http://books.zuri.team",
+    linkName: "Zuri Books",
+    id: "books",
+    redirect: "visit now",
+    pitchText:
+      "Are you starting out in Tech? or you just want to horn your technology skills, check out these books to improve your Design and Coding skills.",
+  },
+  {
+    link: "https://books.zuri.team/python-for-beginners?ref_id=<mysticwillz>",
+    linkName: "python books",
+    redirect: "shop now",
+    pitchText:
+      "Do you want to learn python? Python is one of the most popular programming language, unlock your inner Chakra with our biginner python books",
+  },
+  {
+    link: "https://background.zuri.team",
+    linkName: "background check for coders",
+    id: "pitch",
+    redirect: "see now",
+    pitchText:
+      " We do the work and you reap the gain, Tired of hiring low Quality Engineers? let's help you get it right this time. ",
+  },
+  {
+    link: "https://books.zuri.team/design-rules",
+    linkName: "design books",
+    id: "book__design",
+    redirect: "visit here",
+    pitchText:
+      "Kickstart your design journey with this book created to by Zuri Team for aspiring designers, and the best part it's free!!",
+  },
+];
 
-]
+export default data;
