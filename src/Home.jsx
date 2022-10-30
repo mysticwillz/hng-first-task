9db54eb076ea55581943504b1ad1c6a7aafe09c8
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-30 21:00:00 +0100

    restructured the codebase and added share button

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,11 +7,21 @@ import slack from "./assets/slack.jpg";
 import text from "./assets/text.jpg";
 import zuri from "./assets/zuri.jpg";
 
+import share from "./assets/share-button.jpg";
+
 function Home() {
-  const [selected, setSelected] = useState(null);
+  const [select, setSelect] = useState(null);
 
   return (
-    <main className="max-w-4xl mx-auto">
+    <main className="max-w-4xl mx-auto mt-[30px]">
+      <div className="flex justify-end w-full pr-[10%] mt-[10px]">
+        <img
+          src={share}
+          alt="share button"
+          className="w-[40px] h-[40px] rounded-full cursor-pointer"
+        />
+      </div>
+
       <section className="flex flex-col justify-center items-center mt-[25px]">
         <img
           src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4"
@@ -20,51 +30,58 @@ function Home() {
           className="w-[45px] h-[45px] rounded-full"
         />
 
-        <p className="text-sm font-bold block mt-[10px]">Eze Williams</p>
+        <p className="text-sm font-bold block mt-[10px]">mysticwillz</p>
       </section>
-      <section className="max-w-4xl flex flex-col justify-center mx-auto p-4 items-center">
+      <section className="max-w-4xl mx-auto p-4 ">
         {data.map((x, i) => {
-          const { link, linkName, id, pitchText, redirect } = x;
+          const { link, linkName, id, pitchText } = x;
 
-          const handleClicked = (index) => {
-            if (selected === index) {
-              setSelected(null);
+          const handleMouseEnter = (index) => {
+            if (select === index) {
+              setSelect(null);
             } else {
-              setSelected(index);
+              setSelect(index);
             }
           };
 
+          const handleMouseLeave = () => {
+            return setSelect(null);
+          };
+
           return (
-            <div key={id} className="w-full">
-              {redirect ? (
-                <button
-                  className=" w-full p-4 bg-gray-300 my-[4px] h-[30px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]"
-                  onClick={() => {
-                    handleClicked(i);
+            <div
+              key={id}
+              className="w-full  flex flex-col justify-center mx-auto  items-center "
+            >
+              {pitchText ? (
+                <a
+                  href={link}
+                  onMouseEnter={() => {
+                    handleMouseEnter(i);
                   }}
+                  onMouseLeave={() => {
+                    handleMouseLeave();
+                  }}
+                  className="w-full"
                 >
-                  {linkName}
-                </button>
+                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[50px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]">
+                    {linkName}
+                  </button>
+                </a>
               ) : (
                 <a href={link} className="w-full">
-                  <button className=" w-full p-4 bg-gray-300 my-[4px] h-[30px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]">
+                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[50px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]">
                     {linkName}
                   </button>
                 </a>
               )}
+
               <p
                 className={
-                  selected === i
-                    ? " w-full flex flex-col items-start"
-                    : "hidden"
+                  select === i ? " w-full flex flex-col items-start" : "hidden"
                 }
               >
                 {pitchText}
-                {redirect && (
-                  <span className="w-[5.3rem] h-[2.2rem] whitespace-nowrap cursor-pointer rounded-sm flex items-center justify-center bg-red-500 hover:bg-red-700 text-white capitalize transition duration-250 all ease-in-out my-[1.5px]">
-                    <a href={link}>{redirect}</a>
-                  </span>
-                )}
               </p>
             </div>
           );
@@ -74,7 +91,7 @@ function Home() {
           mysticwillz
         </p>
       </section>
-      <section className="flex justify-center items-center text-lg ">
+      <section className="flex justify-center items-center text-lg  my-0">
         <a href="https://slack.com/mysticwillz">
           <img
             src={slack}
