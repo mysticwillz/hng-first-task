6166f559f9ce81b0d7fe2f947523b79e4fb4e0bd
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-10-31 20:59:54 +0100

    restructured the code and added more btn on mobile

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import { DiGithubBadge } from "react-icons/di";
 
 import ingforgood from "./assets/I4G.png";
+import image from "./assets/image.png";
+import imagehover from "./assets/imagehover.png";
+
+import mobileshare from "./assets/mobileshare.png";
+import sharehover from "./assets/sharehover.png";
+import tooltip from "./assets/Tooltip.png";
+
 import slack from "./assets/slack.jpg";
 import text from "./assets/text.jpg";
 import zuri from "./assets/zuri.jpg";
@@ -11,30 +18,61 @@ import share from "./assets/share-button.jpg";
 
 function Home() {
   const [select, setSelect] = useState(null);
+  const [shareBtn, setShareBtn] = useState(false);
+  const [imagePic, setImagePic] = useState(true);
 
   return (
-    <main className="max-w-4xl mx-auto mt-[30px]">
+    <main className="max-w-6xl mx-auto mt-[30px]">
       <div className="flex justify-end w-full pr-[10%] mt-[10px]">
+        <img
+          src={mobileshare}
+          alt="share button "
+          className="mobile-share md:hidden"
+        />
+        {shareBtn && (
+          <img src={tooltip} alt="share button " className="hidden md:block" />
+        )}
         <img
           src={share}
           alt="share button"
-          className="w-[40px] h-[40px] rounded-full cursor-pointer"
+          onMouseEnter={() => {
+            setShareBtn(true);
+          }}
+          onMouseLeave={() => {
+            setShareBtn(false);
+          }}
+          className="w-[40px] h-[40px] rounded-full cursor-pointer hidden md:block web-share"
         />
       </div>
 
       <section className="flex flex-col justify-center items-center mt-[25px]">
-        <img
-          src="https://avatars.githubusercontent.com/u/111116636?s=400&u=0c9e7cf14378b1d318240ede1d4a9f314fe5c872&v=4"
-          alt="profile"
-          id="profile-id"
-          className="w-[45px] h-[45px] rounded-full"
-        />
-
+        {imagePic ? (
+          <img
+            src={image}
+            alt="profile"
+            onMouseEnter={() => {
+              setImagePic(false);
+            }}
+            s
+            id="profile-id"
+            className="w-[65px] h-[65px] rounded-full cursor-pointer"
+          />
+        ) : (
+          <img
+            src={imagehover}
+            alt="profile"
+            onMouseLeave={() => {
+              setImagePic(true);
+            }}
+            id="profile-id"
+            className="w-[65px] h-[65px] rounded-full cursor-pointer "
+          />
+        )}
         <p className="text-sm font-bold block mt-[10px]">mysticwillz</p>
       </section>
-      <section className="max-w-4xl mx-auto p-4 ">
+      <section className="max-w-6xl mx-auto p-4 ">
         {data.map((x, i) => {
-          const { link, linkName, id, pitchText } = x;
+          const { link, linkName, pitchText } = x;
 
           const handleMouseEnter = (index) => {
             if (select === index) {
@@ -50,7 +88,7 @@ function Home() {
 
           return (
             <div
-              key={id}
+              key={i}
               className="w-full  flex flex-col justify-center mx-auto  items-center "
             >
               {pitchText ? (
@@ -64,13 +102,13 @@ function Home() {
                   }}
                   className="w-full"
                 >
-                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[50px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]">
+                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[60px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out  ">
                     {linkName}
                   </button>
                 </a>
               ) : (
                 <a href={link} className="w-full">
-                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[50px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out transform hover:translate-y-[1px]">
+                  <button className=" hover:bg-gray-500 w-full p-4 bg-gray-300 my-[4px] h-[60px] whitespace-nowrap flex items-center justify-center capitalize rounded text-sm hover:shadow-sm transition duration-150 ease-in-out ">
                     {linkName}
                   </button>
                 </a>
@@ -100,12 +138,13 @@ function Home() {
           />
         </a>
         <a href="https://github.com/mysticwillz/hng-first-task">
-          <DiGithubBadge className="text-[2rem] cursor-pointer px-[2px]" />
+          <DiGithubBadge className="text-[2rem] cursor-pointer px-[4px]" />
         </a>
       </section>
-      <footer className=" md:flex justify-between items-center mt-[50px] p-4">
+      <div className="w-full bg-gray-400 h-[2px] px-4  mt-[50px]"></div>
+      <footer className="  md:flex justify-between items-center  p-4">
         <img src={zuri} alt="zuri" className=" h-[25px]" />
-        <img src={text} alt="text" className="my-[5px] h-[10px]" />
+        <img src={text} alt="text" className="my-[8px] h-[15px]" />
         <img src={ingforgood} alt="i4g" className=" h-[25px]" />
       </footer>
     </main>
