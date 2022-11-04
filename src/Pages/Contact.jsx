c5dc94f5f03cfb80a6f38bd7f3abfdbb513c4bf8
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-11-04 11:56:13 +0100

    set up error message for textarea

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -8,13 +8,19 @@ export default function Contact() {
     last_name: "",
     email: "",
     message: "",
-    checkbox: "",
+    checkbox: false,
   });
   const [check, setCheck] = useState(false);
+  const [error, setError] = useState(false);
   const { first_name, last_name, email, message, checkbox } = formData;
 
   const handleChange = (e) => {
     setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    message === " ".trim() && setError(true);
   };
   return (
     <main className="  bg-white w-full  h-full">
@@ -28,7 +34,12 @@ export default function Contact() {
               Hi there, contact me to ask me anything you have in mind.
             </p>
           </header>
-          <form className="w-full ">
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+            className="w-full "
+          >
             <div className="pt-5 mt-[16px] flex justify-between flex-wrap md:flex-nowrap items-center w-full">
               <div className="w-full md:w-[50%] md:mr-4 mt-[16px]  ">
                 <p className="font-medium leading-[20px] text-gray-700 text-sm">
@@ -73,7 +84,7 @@ export default function Contact() {
                 onChange={(e) => {
                   handleChange(e);
                 }}
-                className="mt-[3px]focus:border-[#84CAFF] border focus:shadow border-gray-300 py-[10px] w-full px-[14px] h-[44px] rounded-[8px] outline-0 "
+                className="mt-[3px]focus:border-[#84CAFF] border focus:border-[#84CAFF] focus:shadow border-gray-300 py-[10px] w-full px-[14px] h-[44px] rounded-[8px] outline-0 "
               />
             </div>
             <div className="w-[100%] mt-[16px]">
@@ -87,23 +98,37 @@ export default function Contact() {
                 onChange={(e) => {
                   handleChange(e);
                 }}
-                className=" mt-[3px] focus:border-[#84CAFF] border focus:shadow border-gray-300 py-[12px] w-full px-[14px] h-[132px] rounded-[8px] outline-0 "
+                className={
+                  error
+                    ? "  mt-[3px] focus:border-[#84CAFF] border focus:shadow border-[#F89687] py-[12px] w-full px-[14px] h-[132px] rounded-[8px] outline-0 "
+                    : "  mt-[3px] focus:border-[#84CAFF] border focus:shadow border-gray-300 py-[12px] w-full px-[14px] h-[132px] rounded-[8px] outline-0 "
+                }
               ></textarea>
+              {error && (
+                <p className="font-medium text-sm text-[#F83F23]">
+                  Please enter a message
+                </p>
+              )}
             </div>
 
             <div
               onClick={() => {
                 setCheck(!check);
               }}
-              htmlFor="checkbox"
-              className=" flex items-center justify-start font-[400px] leading-[24px] "
+              className=" flex items-center cursor-pointer mt-[16px] font-[400px] leading-[24px] "
             >
-              <div className=" flex items-center justify-center hover:shadow hover:border-[#84CAFF] h-[20px] w-[20px] mt-[16px] border rounded-[6px] border-gray-300  ">
+              <div
+                className={
+                  check
+                    ? ` border-[2px] border-[#1570EF] shadow-lg   flex items-center justify-center  h-[20px] w-[20px] mr-2 rounded-[6px]  `
+                    : "hover:shadow flex items-center justify-center hover:border-[#84CAFF] h-[20px] w-[20px] mr-2 border-[2px] rounded-[6px] border-gray-300 "
+                }
+              >
                 <BsCheck
                   className={
                     check
-                      ? "w-[20px] h-[20px] rounded-[6px] text-[#84caff]"
-                      : "hidden border-[2px] border-[#1570EF] shadow-lg "
+                      ? "w-[20px] h-[20px] rounded-[6px] text-[#1570EF]"
+                      : "hidden  "
                   }
                 />
               </div>
@@ -122,7 +147,7 @@ export default function Contact() {
             </div>
             <button
               id="btn__submit"
-              className="h-[48px] mt-[16px] w-full bg-[#1570EF] flex items-center justify-center rounded-[8px] hover:bg-[#175CD3] hover:shadow-xl text-white leading-[24px] font-semibold"
+              className="h-[48px] mt-[16px] w-full bg-[#1570EF] flex items-center justify-center rounded-[8px] hover:bg-[#175CD3] hover:shadow-xl text-white border-0 leading-[24px] font-semibold"
             >
               Send message
             </button>
