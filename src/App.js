a2607b909bae3b59b80fe97cb8609fa81a44c555
EZE WILLIAMS <ezewilliamsezebuilo1010@gmail.com>
2022-11-04 15:05:56 +0100

    imported Inter fonts from google fonts

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       </Routes>
 
       <ToastContainer
-        position="bottom-center"
+        position="top-center"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
